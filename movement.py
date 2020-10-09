531161d2f23420933fd8ff02803612a45f27cd9b
Mateo Llano <mateo.llano1@gmail.com>
2020-10-08 20:37:22 -0500

    code refact

--- movement.py
+++ movement.py
@@ -18,8 +18,8 @@ class Movement:
         # Define HSV colour range for green colour objects
         self.greenLower = (29, 86, 6)
         self.greenUpper = (64, 255, 255)
-        self.up_frame = cv2.imread('imgs/fondo1.png')
-        self.down_frame = cv2.imread('imgs/up.png')
+        self.up_frame = self.load_image('imgs/fondo1.png')
+        self.down_frame = self.load_image('imgs/up.png')
         # Used in deque structure to store no. of given self.buffer points
         self.buffer = 20
 
@@ -41,12 +41,9 @@ class Movement:
             frame = cv2.flip(frame, 1)
             # Resize the given frame to a 600*600 window
             frame = imutils.resize(frame, width=500)
-            # Blur the frame using Gaussian Filter of kernel size 5, to remove excessivve noise
-            blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-            # Convert the frame to HSV, as HSV allow better segmentation.
-            hsv_converted_frame = cv2.cvtColor(
-                blurred_frame, cv2.COLOR_BGR2HSV)
 
+            hsv_converted_frame = self.filter_techniques(frame)
+           
             # Create a mask for the frame, showing green values
             mask = cv2.inRange(hsv_converted_frame, self.greenLower, self.greenUpper)
             # Erode the masked output to delete small white dots present in the masked image
@@ -71,12 +68,8 @@ class Movement:
                 # Find the center of the circle, and its radius of the largest detected contour.
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
 
-                # Calculate the centroid of the ball, as we need to draw a circle around it.
-                M = cv2.moments(c)
-                center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
-                # print("x: " + str(x))
-                # print("y: " + str(y))
-                # print("radius: " + str(radius))
+                self.centroid_calculate(c)
+
                 if x > 210 and y < 276:
                     pyautogui.press("right")
                 if x < 300 and y < 276:
@@ -95,7 +88,6 @@ class Movement:
 
             # Show the output frame
             alpha = 0.5
-            foreground = np.ones((100, 100, 3), dtype='uint8')*255
             added_image = cv2.addWeighted(frame[0:370, 0:500, :], alpha, self.up_frame[0:370, 0:500, :], 1-alpha, 0)
             frame[0:370, 0:500] = added_image
 
@@ -105,6 +97,26 @@ class Movement:
             # If q is pressed, close the window
             if(key == ord('q')):
                 self.quit()
+
+    def centroid_calculate(self, c):
+        # Calculate the centroid of the ball, as we need to draw a circle around it.
+        M = cv2.moments(c)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+        return center
+
+
+    def load_image(self, image_url):
+        return cv2.imread(image_url)
+
+
+    def filter_techniques(self,frame):
+        # Blur the frame using Gaussian Filter of kernel size 5, to remove excessivve noise
+        blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        # Convert the frame to HSV, as HSV allow better segmentation.
+        hsv_converted_frame = cv2.cvtColor(
+            blurred_frame, cv2.COLOR_BGR2HSV)
+        return hsv_converted_frame
+
 
     def quit(self):
         self.video_capture.release()
